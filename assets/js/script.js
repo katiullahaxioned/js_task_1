e30c731faf683e2d0202219e3292dda18d1c8ba4
katiullahaxioned <atiullahk@axioned.com>
2022-08-12 10:55:05 +0530

    js : function added for common functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,39 @@
 var email_placeholder = document.querySelectorAll(".email span");
 var pass_placeholder = document.querySelectorAll(".password span");
 
-for (var i = 0; i < email_placeholder.length; i++) {
-  email_placeholder[i].style.transitionDelay = `${i * (email_placeholder.length / 100)}s`;
+function placeHolderDelay(placeHolder) {
+  for (var i = 0; i < placeHolder.length; i++) {
+    placeHolder[i].style.transitionDelay = `${i * (placeHolder.length / 200)}s`;
+  }
 }
 
-for (var i = 0; i < pass_placeholder.length; i++) {
-  pass_placeholder[i].style.transitionDelay = `${i * (pass_placeholder.length / 200)}s`;
-}
+placeHolderDelay(email_placeholder);
+placeHolderDelay(pass_placeholder);
 
 var email = document.getElementsByName("email")[0];
 var password = document.getElementsByName("password")[0];
-var input_group = document.querySelectorAll(".input-group");
+var emailInputGroup = document.querySelector(".email-group");
+var passInputGroup = document.querySelector(".password-group");
 
-email.addEventListener("focusin", function () {
-  email_placeholder.forEach(function (elem) {
-    elem.classList.add("active");
-    input_group[0].classList.add("active");
+function placeHolderAnim(inputField, placeHolder, inputGroup) {
+  inputField.addEventListener("focusin", function () {
+    placeHolder.forEach(function (element) {
+      element.classList.add("active");
+      inputGroup.classList.add("active");
+    });
   });
-});
+  inputField.addEventListener("focusout", function () {
+    placeHolder.forEach(function (element) {
+      if (inputField.value != "") {
+        element.classList.add("active");
+        inputGroup.classList.add("active");
+      } else {
+        element.classList.remove("active");
+        inputGroup.classList.remove("active");
+      }
+    });
+  });
+}
 
-email.addEventListener("focusout", function () {
-  email_placeholder.forEach(function (elem) {
-    if (email.value != "") {
-      elem.classList.add("active");
-      input_group[0].classList.add("active");
-    } else {
-      elem.classList.remove("active");
-      input_group[0].classList.remove("active");
-    }
-  });
-});
-
-password.addEventListener("focusin", function () {
-  pass_placeholder.forEach(function (elem) {
-    elem.classList.add("active");
-    input_group[1].classList.add("active");
-  });
-});
-
-password.addEventListener("focusout", function () {
-  pass_placeholder.forEach(function (elem) {
-    if (password.value != "") {
-      elem.classList.add("active");
-      input_group[1].classList.add("active");
-    } else {
-      elem.classList.remove("active");
-      input_group[1].classList.remove("active");
-    }
-  });
-});
+placeHolderAnim(email, email_placeholder, emailInputGroup);
+placeHolderAnim(password, pass_placeholder, passInputGroup);
